3f080d3e244c0d731e1344822794f6f6254bca35
AurelBouchet <aurelien-bouchet@hotmail.fr>
2023-05-04 16:35:40 +0200

    java tutorial 1

---
+++ src/main/java/com/aurelien/projet1/controller/DoctorController.java
@@ -0,0 +1,26 @@
+package com.aurelien.projet1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+  @GetMapping("/doctor/1")
+@ResponseBody()
+public String index1() {
+  return "David Tennant";
+}
+
+@GetMapping("/doctor/2")
+@ResponseBody()
+public String index2() {
+  return "William Hartnell";
+}
+
+@GetMapping("/doctor/3")
+@ResponseBody()
+public String index3() {
+  return "Jodie Whittaker";
+}
+}
